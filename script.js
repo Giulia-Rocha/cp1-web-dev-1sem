2e6c2d582f61df040d8eb134c1fb6645f723b615
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-03-26 20:10:16 -0300

    Fazendo o primeiro execicio, -Gustavo

---
+++ script.js
@@ -0,0 +1,17 @@
+//EX01
+
+/*
+//VARIAVEL COM VALOR UNDEFINED
+let nome;
+console.log(nome);
+
+
+//VARIAVEL COM VALOR VAZIO
+let vazio = '';
+console.log(vazio);
+
+
+//VARIAVEL COM VALOR
+let valor = "10";
+console.log(valor);
+*/
