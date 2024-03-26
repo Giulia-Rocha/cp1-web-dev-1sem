b6b538ea508bac4350c4a9a40d7f638d6258dc00
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-03-26 20:12:06 -0300

    Fazendo o segundo exercicio com operadores, -Felipe

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ console.log(vazio);
 let valor = "10";
 console.log(valor);
 */
+
+//EX02
+
+/*
+//OPERADOR !
+let a = 10;
+let b = 5;
+let c = 5;
+console.log(a != b); 
+console.log(b != c);
+
+
+//OPERADOR ==
+let d = 15;
+let e = 15;
+let f = 10;
+console.log(d == e);
+console.log(e == f);
+
+
+//OEPRADOR >=
+let g = 14;
+let h = 10;
+console.log(g >= h);
+console.log(h >= g);
+*/
